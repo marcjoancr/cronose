1ea132e34cd875ef4227dc5b46a93973e9fed338
josep <josep.oliverr@gmail.com>
2019-11-10 19:11:42 +0100

    Log In Implementation (correcting errors)

--- cronose-front/assets/php/Validator.php
+++ cronose-front/assets/php/Validator.php
@@ -5,7 +5,7 @@ require 'DB.php';
 class Validator {
 
     public static function isValid($user) {
-        if (!($user instanceof User)) return false; 
+        if (!($user instanceof User)) return false;
         $result = DB::selectUser($user->getUsername());
         if (!$result) return false;
         if (!password_verify($user->getPassword(), $result[0][1])) return false;
