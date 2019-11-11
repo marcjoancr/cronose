2785a72b4eade80a0000e9004fda9c2b2cfde26c
josep <josep.oliverr@gmail.com>
2019-11-11 15:18:27 +0100

    LogOut Implementation

--- cronose-front/views/components/nav.php
+++ cronose-front/views/components/nav.php
@@ -23,10 +23,10 @@
     </ul>
 
     <?php if(!isset($_SESSION['user'])) : ?>
-      <button type="button" class="btn btn-dark btn-lg login">LOG IN</button>
-      <button type="button" class="btn btn-secondary btn-lg register">REGISTER</button>
+      <a href="../../views/login.php"><button type="button" class="btn btn-dark btn-lg login">LOG IN</button></a>
+      <a href="../../views/register.php"><button type="button" class="btn btn-secondary btn-lg register">REGISTER</button></a>
     <?php else : ?>
-      <button type="button" class="btn btn-secondary btn-lg">LOG OUT</button>
+      <a href="../../assets/php/Logout.php"><button type="button" class="btn btn-secondary btn-lg">LOG OUT</button></a>
     <?php endif ?>
 
   </div>
@@ -38,11 +38,11 @@
       const selector = $(this).val;
       selector.value = "<?= isset($_SESSION['lang']) ? $_SESSION['lang'] : substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2); ?>";
     });
-    $(".login").click(function(){
-      location.href = "login.php";
-    });
-    $(".register").click(function(){
-      location.href = "register.php";
-    });
+    // $(".login").click(function(){
+    //   location.href = "login.php";
+    // });
+    // $(".register").click(function(){
+    //   location.href = "register.php";
+    // });
   });
 </script>
