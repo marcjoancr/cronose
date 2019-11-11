ede2ccf351bfd6049264bd98ece338abd275d173
Marc Joan Cabrer Rosselló <marcjoancr@gmail.com>
2019-11-11 13:41:03 +0100

    Space tabs and control flow in nav.php

--- cronose-front/views/components/nav.php
+++ cronose-front/views/components/nav.php
@@ -1,50 +1,48 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a class="navbar-brand" href="#">CRONOSE</a>
+  <a class="navbar-brand" href="#">CRONOSE</a>
 
-      <div class="collapse navbar-collapse" id="language">
-          <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                  <button class="btn" href="database.php"><i class="fa fa-database"></i> SERVICES</button>
-              </li>
-              <li class="nav-item active">
-                  <button class="btn" href="aboutUs.php"><i class="fa fa-address-card"></i> ABOUT US</button>
-              </li>
-          </ul>
-        <ul class="navbar-nav mr-auto" id="language_selector" name="lang" target="_self">
-          <li class="nav-item active">
-            <a class="nav-link border rounded text-dark language" value="es"><strong>ESPAÑOL</strong><span class="sr-only">(Current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link border rounded text-dark language" value="ca"><strong>CATALÀ</strong></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link border rounded text-dark language" value="en"><strong>ENGLISH</strong></a>
-          </li>
-        </ul>
+  <div class="collapse navbar-collapse" id="language">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <button class="btn" href="database.php"><i class="fa fa-database"></i> SERVICES</button>
+        </li>
+        <li class="nav-item active">
+            <button class="btn" href="aboutUs.php"><i class="fa fa-address-card"></i> ABOUT US</button>
+        </li>
+    </ul>
+    <ul class="navbar-nav mr-auto" id="language_selector" name="lang" target="_self">
+      <li class="nav-item active">
+        <a class="nav-link border rounded text-dark language" value="es"><strong>ESPAÑOL</strong><span class="sr-only">(Current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link border rounded text-dark language" value="ca"><strong>CATALÀ</strong></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link border rounded text-dark language" value="en"><strong>ENGLISH</strong></a>
+      </li>
+    </ul>
 
-          <?php if(!isset($_SESSION['user'])) {
-            echo '<button type="button" class="btn btn-dark btn-lg login">LOG IN</button>';
-            echo '<button type="button" class="btn btn-secondary btn-lg register">REGISTER</button>';
-          } else {
-              echo '<button type="button" class="btn btn-secondary btn-lg">LOG OUT</button>';
-          }?>
+    <?php if(!isset($_SESSION['user'])) : ?>
+      <button type="button" class="btn btn-dark btn-lg login">LOG IN</button>
+      <button type="button" class="btn btn-secondary btn-lg register">REGISTER</button>
+    <?php else : ?>
+      <button type="button" class="btn btn-secondary btn-lg">LOG OUT</button>
+    <?php endif ?>
 
-      </div>
+  </div>
 </nav>
 
-  <script>
-
-      $(document).ready(function(){
-          $("a.language").click(function() {
-              const selector = $(this).val;
-              selector.value = "<?= isset($_SESSION['lang']) ? $_SESSION['lang'] : substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2); ?>";
-          });
-          $(".login").click(function(){
-              location.href = "login.php";
-          });
-          $(".register").click(function(){
-              location.href = "register.php";
-          });
-      });
-
-  </script>
+<script>
+  $(document).ready(function(){
+    $("a.language").click(function() {
+      const selector = $(this).val;
+      selector.value = "<?= isset($_SESSION['lang']) ? $_SESSION['lang'] : substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2); ?>";
+    });
+    $(".login").click(function(){
+      location.href = "login.php";
+    });
+    $(".register").click(function(){
+      location.href = "register.php";
+    });
+  });
+</script>
