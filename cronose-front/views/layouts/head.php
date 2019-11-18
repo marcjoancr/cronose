bb43341ec1632008110c3aca2183b71ffb1b8970
Marc Joan Cabrer Rosselló <marcjoancr@gmail.com>
2019-11-18 21:06:54 +0100

    Adding utf-8 charset

--- cronose-front/views/layouts/head.php
+++ cronose-front/views/layouts/head.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
-  <link rel="shortcut icon" href="favicon.ico"/>
-  <meta charset="utf-8">
+  <link rel="shortcut icon" href="/assets/img/favicon.ico">
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
   <title>Cronose</title>
   <!-- CSS -->
   <link rel="stylesheet" href="/assets/plugin/bootstrap/bootstrap.min.css">
   <link rel="stylesheet" href="/assets/stylesheet/css/main.css">
-  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet"/>
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
   <!-- SCRIPTS -->
   <script src="/assets/plugin/jquery/jquery-3.4.1.min.js"></script>
   <script src="/assets/plugin/js/popper.min.js"></script>
