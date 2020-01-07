f07e7e4011a1eff43fe88dab53753d7aca539eba
Marc Joan Cabrer Rosselló <marcjoancr@gmail.com>
2020-01-07 16:58:30 +0100

    Index URL Controller from Front to API

--- cronose-front/index.php
+++ cronose-front/index.php
@@ -1,7 +1,29 @@
-<?php
+<?php require_once 'config.inc.php'; ?>
 
-	require $_SERVER['DOCUMENT_ROOT'] . '/controllers/Language.controller.php';
-  require $_SERVER['DOCUMENT_ROOT'] . '/controllers/Works.controller.php';
-  require $_SERVER['DOCUMENT_ROOT'] . '/controllers/Profile.controller.php';
-  require_once $_SERVER['DOCUMENT_ROOT'] . '/assets/php/User.php';
-  //require_once './routes/api.route.php';
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script>
+    $.ajax({
+      type: "GET",
+      url: "<?= $config['api']['api_url'] ?>" + window.location.pathname,
+      dataType: "html"
+    }).done((data) => {
+      $('#app').html(data);
+      console.log(data);
+      
+    });
+  </script>
+</head>
+
+<body>
+  <div id="app"></div>
+</body>
+
+</html>
