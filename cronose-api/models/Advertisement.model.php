4f0df2521cc8c6ed7331b3abe7f8cc7f39968526
Pedro <peri.b182@gmail.com>
2020-01-08 10:29:12 +0100

    Advertisement model

---
+++ cronose-api/models/Advertisement.model.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once 'Model.php';
+
+class AdvertisementModel extends Model
+{
+
+  public function __construct($id) {
+    $this->schema = array(
+      'id' => $id
+    );
+    parent::__construct();
+  }
+
+  public static function modelValidation($body) {
+    $body = json_decode($body);
+    return true;
+  }
+
+  public static function getAll() {
+    $sql = "SELECT * FROM Advertisement";
+    $statement = self::$DB->prepare($sql);
+    $statement->execute();
+    return $statement->fetchAll();
+  }
+
+  public static function getById($company, $specialization) {
+    $sql = "SELECT * FROM Advertisement WHERE company_id = " . $company . " AND specialization_id = " . $specialization . ";";
+    $statement = self::$DB->prepare($sql);
+    $statement->execute();
+    return $statement->fetchAll();
+  }
+
+  public static function getAllByLanguage($language) {
+    $sql = "SELECT * FROM advertisement_language WHERE language_id = " . $language . ";";
+    $statement = self::$DB->prepare($sql);
+    $statement->execute();
+    return $statement->fetchAll();
+  }
+
+  public static function getAllBySpecialization($specialization) {
+    $sql = "SELECT * FROM advertisement_language WHERE specialization_id = " . $specialization . ";";
+    $statement = self::$DB->prepare($sql);
+    $statement->execute();
+    return $statement->fetchAll();
+  }
+
+}
