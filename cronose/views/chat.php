bc90eab6d3ed4225f1671244585045f5424e7fc3
josep <josep.oliverr@gmail.com>
2020-01-14 09:47:45 +0100

    Implementing style on chat

--- cronose/views/chat.php
+++ cronose/views/chat.php
@@ -1,13 +1,47 @@
 <?php require 'layouts/head.php';?>
-
+<style type="text/css">
+  .scroll {
+    max-height: 400px;
+    overflow-y: auto;
+}
+  .chatBox {
+    height: 400px;
+  }
+</style>
 <h1>Chat</h1>
 
-<div id="chat"></div>
-<div class="input-group">
-  <span class="input-group-btn">
-    <button id="send-btn" class="btn btn-secondary" type="button"><i class="far fa-paper-plane"></i></button>
-  </span>
-  <input id="message" type="text" class="form-control">
+
+
+<div class="container mt-5">
+  <div class="row justify-content-center w-100 ">
+    <div class="col-md-8 mb-4">
+      <div class="card w-100" id="myForm">
+        <div class="card-header white p-2" id="toggle">
+          <div class="d-flex">
+            <div class="profile-photo">
+              <img src="https://cdn.pixabay.com/photo/2012/04/26/19/43/profile-42914_960_720.png" alt="..." width="80" class="mr-3 rounded-circle img-thumbnail shadow-sm">
+            </div>
+            <div class="data">
+              <p class="name mb-0"><strong id="name"></strong></p>
+              <p class="activity text-muted mb-0">Active now</p>
+            </div>
+          </div>
+        </div>
+        <div class="my-custom-scrollbar">
+          <div class="card-body p-3">
+            <div class="chat-message scroll chatBox" id="chat">
+
+            </div>
+          </div>
+        </div>
+        <div class="input-group">
+          <span class="input-group-btn">
+            <button id="send-btn" class="btn btn-secondary" type="button"><i class="far fa-paper-plane"></i></button>
+          </span>
+          <input id="message" type="text" class="form-control">
+        </div>
+    </div>
+  </div>
 </div>
 
 <a href="/wallet">wallet</a><br>
@@ -18,9 +52,17 @@
   $(document).ready(function(){
 
     show();
+    $('#name').html(window.location.pathname.split('/')[3]);
+
+    $("#message").keypress(function(event) {
+      if (event.keyCode === 13) {
+          $("#send-btn").click();
+      }
+    });
 
     $('#send-btn').click(() => {
-      send();
+      console.log($.trim($('#message').val()));
+      if (!$.trim($('#message').val()) == "") send();
     });
 
     setInterval(function () {
@@ -40,9 +82,13 @@
         success: (data) => {
           if (data.status == 'success') {
             let msg = data.msg;
-            htmlLi = "<ul>"
+            htmlLi = '<ul class="list-unstyled chat">';
             $.each (msg, function(key, value) {
-              htmlLi += '<li><strong>' + value['name'] + ': </strong>' + value['message'] + '</li>';
+              if (value['name'] == window.location.pathname.split('/')[3]) {
+                htmlLi += '<div class="card bg-light rounded w-75 z-depth-0 mb-1"><div class="card-body p-2"><p class="card-text black-text"> ' + value['message'] + ' </p></div></div>';
+              } else {
+                htmlLi += '<div class="card bg-primary rounded w-75 float-right z-depth-0 mb-1"><div class="card-body p-2"><p class="card-text text-white">' + value['message'] + '</p></div></div>';
+              }
             });
             htmlLi += "</ul>"
             document.getElementById("chat").innerHTML = htmlLi;
@@ -65,7 +111,7 @@
         dataType: 'json',
         data: { reciver, msg },
         success: (data) => {
-          // console.log(data)
+          $('#message').val('');
           show();
         },
         error: ((data) => {
