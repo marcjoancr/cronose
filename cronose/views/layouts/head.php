c1e23d6943464d510d64d438ecc8533aaaa8a181
Marc Joan Cabrer Rosselló <marcjoancr@gmail.com>
2020-01-22 08:16:30 +0100

    Google Analytics Script

--- cronose/views/layouts/head.php
+++ cronose/views/layouts/head.php
@@ -13,6 +13,14 @@
   <link rel="stylesheet" href="/assets/stylesheet/css/main.css">
   <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
   <link rel="stylesheet" href="/assets/plugin/leaflet/leaflet.css">
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-HFWQCBNSPH"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-HFWQCBNSPH');
+  </script>
   <!-- SCRIPTS -->
   <script src="/assets/plugin/jquery/jquery-3.4.1.min.js"></script>
   <script src="/assets/plugin/js/popper.min.js"></script>
