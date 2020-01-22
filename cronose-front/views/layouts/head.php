e384ec4ac4d04b484b935d4827b62865467f9d2e
root <root@grup-c.c.daw-1819.internal>
2020-01-22 08:22:26 +0100

    Changes from server

--- cronose-front/views/layouts/head.php
+++ cronose-front/views/layouts/head.php
@@ -4,6 +4,8 @@
   <link rel="shortcut icon" href="/assets/img/favicon.ico">
   <meta http-equiv="content-type" content="text/html; charset=UTF-8">
   <title>Cronose</title>
+  <meta name="title" content="Cronose" />
+  <meta name="description" content="A social platform for sharing your time with other people and let you know to others. Make this place your site for knowing nice other people and interchange some activities eachother."
   <!-- CSS -->
   <link rel="stylesheet" href="/assets/plugin/bootstrap/bootstrap.min.css">
   <link rel="stylesheet" href="/assets/stylesheet/css/main.css">
