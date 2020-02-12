12ec82d63335dc3407d92a39b54f08e9bd4273ef
Miguel Angel <mena_alcalde@hotmail.com>
2020-02-12 11:03:16 +0100

    footer fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from 'react';
 
 export default function Footer() {
-  return (
-    <div>
-      <h1>This is footer</h1>
-    </div>
-  )
+	return (
+		<footer class='page-footer'>
+			<div class='row text-center'>
+				<div class='col-md-3'>
+					<h2 class='pt-2 pb-3'>About Us</h2>
+					<p>
+						<a href='#'>History</a>
+					</p>
+					<p>
+						<a href='#'>Project Info</a>
+					</p>
+					<p>
+						<a href='#'>Team</a>
+					</p>
+				</div>
+				<div class='col-md-3'>
+					<h2 class='pt-2 pb-3'>How it work</h2>
+					<p>
+						<a href='#'>What is a time bank</a>
+					</p>
+					<p>
+						<a href='#'>How it work</a>
+					</p>
+					<p>
+						<a href='#'>Diference</a>
+					</p>
+				</div>
+				<div class='col-md-3'>
+					<h2 class='pt-2 pb-3'>Contact</h2>
+					<p>
+						<a href='#'>How to</a>
+					</p>
+					<p>
+						<a href='#'>Form</a>
+					</p>
+				</div>
+				<div class='col-md-3'>
+					<h2 class='pt-2 pb-3'>FAQ</h2>
+					<p>
+						<a href='#'>FAQ</a>
+					</p>
+				</div>
+			</div>
+		</footer>
+	);
 }
