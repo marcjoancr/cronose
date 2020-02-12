0ab17ac01d4c20e3f46a12c041300e82444ab5bd
josep <josep.oliverr@gmail.com>
2020-02-12 11:26:14 +0100

    Implementing css

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,31 +3,36 @@ import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
 
 export default function SideBar(props) {
 	return (
-		<nav className='sidebar'>
-			<BrowserRouter basename={props.basename}>
-				<ul className='menu'>
-					{props.routes.map((route, index) => (
-						<li key={index} className='item'>
-							<NavLink
-								to={route.path}
+		<div>
+			<nav className='vertical-nav'>
+  			<section className="navbar">
+				<BrowserRouter basename={props.basename}>
+					<ul className='nav flex-column mb-0'>
+						{props.routes.map((route, index) => (
+							<li key={index} className='nav-item pt-3'>
+								<NavLink
+									to={route.path}
+									exact={route.exact}
+									className='btn text-white'
+									activeClassName='active'>
+									{route.title}
+								</NavLink>
+							</li>
+						))}
+					</ul>
+					<Switch>
+						{props.routes.map((route, index) => (
+							<Route
+								key={index}
+								path={route.path}
 								exact={route.exact}
-								activeClassName='active'>
-								{route.title}
-							</NavLink>
-						</li>
-					))}
-				</ul>
-				<Switch>
-					{props.routes.map((route, index) => (
-						<Route
-							key={index}
-							path={route.path}
-							exact={route.exact}
-							component={route.component}
-						/>
-					))}
-				</Switch>
-			</BrowserRouter>
-		</nav>
+								component={route.component}
+							/>
+						))}
+					</Switch>
+				</BrowserRouter>
+				</section>
+			</nav>
+		</div>
 	);
 }
