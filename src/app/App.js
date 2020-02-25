03964d74cbff84757ebbb67f068014cb09babfe4
Miguel Angel <mena_alcalde@hotmail.com>
2020-02-25 10:25:46 +0100

    scss

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import { SideBar } from '../components/layouts/Nav';
 import Footer from '../components/layouts/Footer';
-import { NavBar, SideBar } from '../components/layouts/Nav';
 
 export default function App(props) {
 	return (
-		<div className='container-side'>
+		<div id='app' className='w-100'>
 			<SideBar
 				basename={props.navigator.basename}
 				routes={props.navigator.routes}
--- src/homepage/HomePage.js
+++ src/homepage/HomePage.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { NavBar, SideBar } from '../components/layouts/Nav';
+import { NavBar } from '../components/layouts/Nav';
+import Footer from '../components/layouts/Footer';
 
 export default function HomePage(props) {
 	return (
-		<>
-			<SideBar 
+		<div id='homepage' className='w-100'>
+			<NavBar
 				basename={props.navigator.basename}
 				routes={props.navigator.routes}
 				refresh={true}
 			/>
-		</>
+			<Footer />
+		</div>
 	);
 }
