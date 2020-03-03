062e7ea8393516336c26e13d859a96fb3021dc62
Miguel Angel <mena_alcalde@hotmail.com>
2020-03-03 10:07:31 +0100

    my works

--- src/app/components/MyWorks.js
+++ src/app/components/MyWorks.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import WorkCard from './WorkCard';
 
 export default function MyWorks() {
-  return (
-    <div>
-      <h1>My Works</h1>
-    </div>
-  )
+	return (
+		<>
+			<div className='text-right pt-4 mr-4'>
+				<a href='#' class='btn'>
+					WORK HISTORY
+				</a>
+			</div>
+			<div className='text-center pt-2'>
+				<h1>My Works</h1>
+			</div>
+			<section className='works'>
+				<WorkCard />
+				<WorkCard />
+				<WorkCard />
+			</section>
+			<div className='text-center'>
+				<a href='#' class='btn btn-lg mb-4'>
+					NEW OFFER
+				</a>
+			</div>
+		</>
+	);
 }
---
+++ src/app/components/WorkCard.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export default function WorkCard() {
+	return (
+		<article className='card my-work-card'>
+			<section className='info row'>
+				<figure className='col-lg-4'>
+					<img
+						class='card-img'
+						src='/assets/img/img-work.jpg'
+						alt='Card image cap'></img>
+				</figure>
+				<div className='col-lg-8'>
+					<section className='header card-header row'>
+						<p className='schedule col-6 text-muted'>HORARIO</p>
+						<div className='valuation col-6 text-right'>VALORACIONES</div>
+					</section>
+					<div className='card-body'>
+						<h4>
+							<b>TITULO</b>
+						</h4>
+						<hr></hr>
+						<p class='card-text'>
+							Lorem ipsum dolor sit amet, ea vel prima adhuc, scripta
+							liberavisse ea quo, te vel vidit mollis complectitur.
+						</p>
+						<section className='text-right'>
+							<p className='price d-inline'>
+								<b>PRECIO</b>
+							</p>
+							<a href='#' id='editButton' class='btn text-white'>
+								Edit Offer
+							</a>
+							<a href='#' class='btn text-white'>
+								See Offer
+							</a>
+						</section>
+					</div>
+				</div>
+			</section>
+		</article>
+	);
+}
