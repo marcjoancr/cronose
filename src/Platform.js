880d375aea0602d0f06a9b5824f9d1a28ffc4d53
Marc Joan Cabrer Rosselló <marcjoancr@gmail.com>
2020-03-05 19:37:08 +0100

    Platform Class to Function

--- src/Platform.js
+++ src/Platform.js
@@ -5,34 +5,25 @@ import App from './app/App';
 import navigators from './configs/navigators.js';
 import LoginContextProvider, { LoginContext } from './contexts/LoginContext';
 
-export default class Platform extends Component {
-	static contextType = LoginContext;
-
-	constructor() {
-		super();
-		this.state = {};
-	}
-
-	render() {
-		return (
-			<LoginContextProvider>
-				<LoginContext.Consumer>
-					{(context) => {
-						if (context.isLogged) {
-							return (
-								<App
-									navigator={navigators.filter((nav) => nav.name == 'app')[0]}
-								/>
-							);
-						}
+export default function Platform() {
+	return (
+		<LoginContextProvider>
+			<LoginContext.Consumer>
+				{(context) => {
+					if (context.isLogged) {
 						return (
-							<HomePage
-								navigator={navigators.filter((nav) => nav.name == 'root')[0]}
+							<App
+								navigator={navigators.filter((nav) => nav.name == 'app')[0]}
 							/>
 						);
-					}}
-				</LoginContext.Consumer>
-			</LoginContextProvider>
-		);
-	}
+					}
+					return (
+						<HomePage
+							navigator={navigators.filter((nav) => nav.name == 'root')[0]}
+						/>
+					);
+				}}
+			</LoginContext.Consumer>
+		</LoginContextProvider>
+	);
 }
