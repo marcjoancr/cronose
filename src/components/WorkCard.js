82ee1d456ca25d2fc9682b53b39bbe0ce0440324
PedroMartinez471 <46518926+PedroMartinez471@users.noreply.github.com>
2020-03-05 11:39:59 +0100

    Card schedule

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -25,8 +25,24 @@ export default function WorkCard(props) {
 				</figure>
 				<div className='col-lg-9'>
 					<section className='header card-header row'>
-						<p className='schedule col-6 text-muted'>HORARIO</p>
-						<div className='valuation col-6 text-right'>
+						<p className='schedule col-2 text-muted my-auto d-none d-md-block'>
+							HORARIO
+						</p>
+						<div className='col-6 d-inline-flex'>
+							<ul className='list-group list-group-horizontal list-unstyled mb-4 my-auto'>
+								<li className='text-muted my-auto'>L</li>
+								<li className=''>M</li>
+								<li className=''>X</li>
+								<li className='text-muted my-auto'>J</li>
+								<li className=''>V</li>
+								<li className=''>S</li>
+								<li className=''>D</li>
+							</ul>
+							<p className='ml-4 my-auto'>
+								De: <b>10:00h</b> a <b>14:00h</b>
+							</p>
+						</div>
+						<div className='valuation col-4 text-right my-auto'>
 							<Rater total={5} rating={rater} interactive={false} />
 						</div>
 					</section>
@@ -38,9 +54,8 @@ export default function WorkCard(props) {
 						<p className='card-text'>{description}</p>
 						<section className='text-right'>
 							<p className='price d-inline'>
-								<b>{price}</b>
+								<b>Precio: {price}</b>
 							</p>
-
 							<a href='/work' className='btn text-white'>
 								See Offer
 							</a>
