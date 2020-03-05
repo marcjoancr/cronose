fddc7abe2ce24e1b30b7746bc844ce0b6372004f
Miguel Angel <mena_alcalde@hotmail.com>
2020-03-05 09:10:24 +0100

    buttons language

--- src/homepage/components/Home.js
+++ src/homepage/components/Home.js
@@ -9,7 +9,10 @@ class Home extends Component {
 	render() {
 		return (
 			<div>
-				<LanguagePicker changeLanguage={this.props.changeLanguage} />
+				<div id='btnLanguages'>
+					<LanguagePicker changeLanguage={this.props.changeLanguage} />
+				</div>
+
 				<div className='container mt-5 pt-4 text-center'>
 					<h1>
 						<Translate string={'share-time'} />
@@ -36,36 +39,3 @@ class Home extends Component {
 }
 
 export default Home;
-
-// export default function Home() {
-// 	return (
-// 		<>
-// 			<div>
-// 				<h1>
-// 					<Translate string={'title-page'} />
-// 				</h1>
-// 				Hola
-// 				<LanguagePicker changeLanguage={this.props.changeLanguage} />
-// 				<AboutUs />
-// 			</div>
-// 			{/* <div className='container mt-5 pt-4 text-center'>
-// 				<h1>Share Your Time</h1>
-// 				<p>
-// 					Lorem Ipsum is simply dummy text of the printing and typesetting
-// 					industry. Lorem Ipsum has been the industry's standard dummy text ever
-// 					since the 1500s, when an unknown printer took a galley of type and
-// 					scrambled it to make a type specimen book.
-// 				</p>
-// 				<a href='/market'>
-// 					<input
-// 						type='button'
-// 						className='btn btn-orange'
-// 						value='See Our Market'></input>
-// 				</a>
-// 			</div>
-// 			<HowItWorks />
-// 			<AboutUs />
-// 			<Contact /> */}
-// 		</>
-// 	);
-// }
