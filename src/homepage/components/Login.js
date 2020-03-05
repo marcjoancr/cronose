df0e5a7a3a7a657a67f0e23855004ae6c207a2f1
Marc Joan Cabrer Rosselló <marcjoancr@gmail.com>
2020-03-05 23:16:07 +0100

    Fixing WorkCard and Login

--- src/homepage/components/Login.js
+++ src/homepage/components/Login.js
@@ -21,56 +21,52 @@ export default class Login extends React.Component {
 
 	render() {
 		return (
-			<LoginContext.Consumer>
-				{(context) => (
-					<div className='card-login card text-center'>
-						<h3 className='card-title text-center'>LOGIN</h3>
-						<form
-							id='login_form'
-							method='post'
-							target='_self'
-							className='form-signin'
-							onSubmit={this.login}>
-							<div className='form-label-group mt-4 text-left'>
-								<label htmlFor='email'>Email</label>
-								<input
-									id='email'
-									type='email'
-									name='email'
-									className='form-control'
-									placeholder='email'
-									required
-									autoFocus
-								/>
-							</div>
-							<div className='form-label-group mt-4 text-left'>
-								<label htmlFor='password'>Password</label>
-								<input
-									id='password'
-									type='password'
-									name='password'
-									className='form-control'
-									placeholder='password'
-									required
-								/>
-							</div>
-							<input
-								className='btn-login btn btn-lg btn-primary btn-block mt-4'
-								type='submit'
-								value='Submit'
-							/>
-						</form>
-						<div className='card-footer'>
-							<div className='justify-content-center links'>
-								Don't have an account? <a href='/register'> Sign Up</a>
-							</div>
-							<div className=' justify-content-center'>
-								<a href='#'>Forgot your password?</a>
-							</div>
-						</div>
+			<div className='card-login card text-center'>
+				<h3 className='card-title text-center'>LOGIN</h3>
+				<form
+					id='login_form'
+					method='post'
+					target='_self'
+					className='form-signin'
+					onSubmit={this.login}>
+					<div className='form-label-group mt-4 text-left'>
+						<label htmlFor='email'>Email</label>
+						<input
+							id='email'
+							type='email'
+							name='email'
+							className='form-control'
+							placeholder='email'
+							required
+							autoFocus
+						/>
 					</div>
-				)}
-			</LoginContext.Consumer>
+					<div className='form-label-group mt-4 text-left'>
+						<label htmlFor='password'>Password</label>
+						<input
+							id='password'
+							type='password'
+							name='password'
+							className='form-control'
+							placeholder='password'
+							required
+						/>
+					</div>
+					<input
+						className='btn-login btn btn-lg btn-primary btn-block mt-4'
+						type='submit'
+						value='Submit'
+					/>
+				</form>
+				<div className='card-footer'>
+					<div className='justify-content-center links'>
+						Don't have an account? <a href='/register'> Sign Up</a>
+					</div>
+					<div className=' justify-content-center'>
+						<a href='#'>Forgot your password?</a>
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
