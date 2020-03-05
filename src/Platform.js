f330cee29b7c7e1ad1f0695bfe9afced019a7856
Marc Joan Cabrer Rosselló <marcjoancr@gmail.com>
2020-03-05 19:03:44 +0100

    Switch between APP and HomePage

--- src/Platform.js
+++ src/Platform.js
@@ -16,16 +16,23 @@ export default class Platform extends Component {
 	render() {
 		return (
 			<LoginContextProvider>
-				<HomePage
-					navigator={navigators.filter((nav) => nav.name == 'root')[0]}
-				/>
+				<LoginContext.Consumer>
+					{(context) => {
+						if (context.isLogged) {
+							return (
+								<App
+									navigator={navigators.filter((nav) => nav.name == 'app')[0]}
+								/>
+							);
+						}
+						return (
+							<HomePage
+								navigator={navigators.filter((nav) => nav.name == 'root')[0]}
+							/>
+						);
+					}}
+				</LoginContext.Consumer>
 			</LoginContextProvider>
-		);
-		const { value } = this.context;
-		value.isLogged ? (
-			<App navigator={navigators.filter((nav) => nav.name == 'app')[0]} />
-		) : (
-			<HomePage navigator={navigators.filter((nav) => nav.name == 'root')[0]} />
 		);
 	}
 }
