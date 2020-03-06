552e3ba361c1486c5646f083bc892d3689f177c9
Marc Joan Cabrer Rosselló <marcjoancr@gmail.com>
2020-03-06 08:32:46 +0100

    Updating Navigator

--- src/components/layouts/Nav.js
+++ src/components/layouts/Nav.js
@@ -33,9 +33,6 @@ function NavBar(props) {
 					<ul id='ul-nav-hor' className='navbar-nav'>
 						{props.routes.map(function(route, index) {
 							if (route.show == false) return;
-							if (route.path === '/newoffer') return false;
-							if (route.path === '/userValidator') return false;
-							if (route.path === '/resetPassword') return false;
 							return (
 								<li key={index} className='item'>
 									<NavLink
@@ -72,13 +69,6 @@ function SideBar(props) {
 					<ul className='nav flex-column mb-0 mt-4'>
 						{props.routes.map(function(route, index) {
 							if (route.show == false) return;
-							if (route.path === '/newoffer') return false;
-							if (route.path === '/home') return false;
-							if (route.path === '/#about') return false;
-							if (route.path === '/#HowItWorks') return false;
-							if (route.path === '/#contact') return false;
-							if (route.path === '/userValidator') return false;
-							if (route.path === '/resetPassword') return false;
 							return (
 								<li key={index} className='nav-item'>
 									<NavLink
